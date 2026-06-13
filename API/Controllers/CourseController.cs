75f12b240a1a86135bf800fc31c3e2e12ead2ecf
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2026-06-13 17:02:06 +0700

    feat: get all course and single course

---
+++ API/Controllers/CourseController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entity;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class CourseController: BaseController
+    {
+        private readonly StoreContext _context;
+
+        public CourseController(StoreContext context)
+        {
+            _context = context;
+        }
+        
+        // GET Course Request
+        [HttpGet]
+        public async Task<ActionResult<List<Course>>> GetCourses()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+        
+        // GET Single Course
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Course>> GetCourse(Guid id)
+        {
+            return await _context.Courses.FindAsync(id);
+        }
+    }
+}
