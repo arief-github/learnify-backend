53437adc6a0c98bb1bf1debc84449a839d8a3045
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2026-06-13 16:20:54 +0700

    initial: config db and seed data

---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            
+            try
+            {
+                var context = services.GetRequiredService<StoreContext>();
+                await context.Database.MigrateAsync();
+                
+                // seed data
+                await StoreContextSeed.SeedAsync(context, logger);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Something wrong happened during migrations");
+            }
+            
+            await host.RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Infrastructure/StoreContextSeed.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILogger logger)
+        {
+            try
+            {
+                if (!context.Courses.Any())
+                {
+                    // 1. read file json secara asynchronous
+                    var coursesData = await File.ReadAllTextAsync("../Infrastructure/Seed/courses.json");
+                    
+                    // 2. konvert json kedalam list courses.
+                    var courses = JsonSerializer.Deserialize <List<Course>> (coursesData);
+
+                    // throw return jika courses null
+                    if (courses == null)
+                    {
+                        return;
+                    }
+                    
+                    // jika hasil konversi cocok, masukkan data ke dalam DB
+                    foreach (var item in courses)
+                    {
+                        context.Courses.Add(item);
+                    }
+                    
+                    // save changes to DB
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                // jika json tidak cocok saat dilakukan convert. throw error exception
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
