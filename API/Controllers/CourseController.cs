0bc456f9e11325eeed616892858dd30f3e9203c1
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2026-06-18 22:58:18 +0700

    refactor(dto): setup dto, mapper for courses

--- API/Controllers/CourseController.cs
+++ API/Controllers/CourseController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using API.DTO;
+using AutoMapper;
 using Entity;
 using Entity.Interfaces;
 using Infrastructure;
@@ -12,10 +14,12 @@ namespace API.Controllers
     public class CourseController: BaseController
     {
         private readonly ICourseRepository _repository;
+        private readonly IMapper _mapper;
 
-        public CourseController(ICourseRepository repository)
+        public CourseController(ICourseRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
         
         // GET Course Request
@@ -23,16 +27,16 @@ namespace API.Controllers
         public async Task<ActionResult<Course>> GetCourses()
         {
             var courses = await _repository.GetCoursesAsync();
-            return Ok(courses);
+            return Ok(_mapper.Map<IReadOnlyList<Course>, IReadOnlyList<CourseDto>>(courses));
         }
         
         // GET Single Course
         [HttpGet("{id}")]
 
-        public async Task<ActionResult<Course>> GetCourse(Guid id)
+        public async Task<ActionResult<CourseDto>> GetCourse(Guid id)
         {
             var course = await _repository.GetCourseByIdAsync(id);
-            return Ok(course);
+            return _mapper.Map<Course, CourseDto>(course);
         }
     }
 }
